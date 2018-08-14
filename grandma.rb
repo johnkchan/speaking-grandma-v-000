d5e5a75534e71c4985f2392fbc238530a8046ddf
Jonathan Chan <johnkchan252@gmail.com>
2018-08-14 01:28:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
     else
-      "HUH?! SPEAK UP, SONNY!"
+      "NO, NOT SINCE 1938!"
     end
   end
 end
